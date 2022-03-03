71b02d53b4c75aa9a8a6c9225e1f07366220d556
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-04 02:20:49 +0500

    Add Form Object, Set LocalStorage and Display Data

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-const menuIcon = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('#close-icon');
-const navBarList = document.querySelectorAll('.navbar-list-mobile li');
-const mobilePopupNav = document.querySelector('#mobile-popup');
+const menuIcon = document.querySelector(".hamburger");
+const closeIcon = document.querySelector("#close-icon");
+const navBarList = document.querySelectorAll(".navbar-list-mobile li");
+const mobilePopupNav = document.querySelector("#mobile-popup");
 
-menuIcon.addEventListener('click', () => {
-  mobilePopupNav.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  mobilePopupNav.classList.toggle("active");
 });
 
 navBarList.forEach((i) => {
-  i.addEventListener('click', () => {
-    mobilePopupNav.classList.toggle('active');
+  i.addEventListener("click", () => {
+    mobilePopupNav.classList.toggle("active");
   });
 });
 
-closeIcon.addEventListener('click', () => {
-  mobilePopupNav.classList.toggle('active');
+closeIcon.addEventListener("click", () => {
+  mobilePopupNav.classList.toggle("active");
 });
 
 // --------FORM VALIDATION----------
 
-const form = document.getElementById('form');
-const small = document.getElementsByTagName('small')[0];
-const emailAddress = document.getElementById('email-address');
+const form = document.getElementById("form");
+const small = document.getElementsByTagName("small")[0];
+const name = document.getElementById("name");
+const email = document.getElementById("email-address");
+const description = document.getElementById("description");
 
 function validateEmail(input) {
   const address = input.toString();
@@ -34,11 +36,28 @@ function validateEmail(input) {
   return isLower;
 }
 
-form.addEventListener('submit', (event) => {
-  if (validateEmail(emailAddress.value)) {
-    small.textContent = '';
+// ---------LOCAL STORAGE-----------// START
+
+const formData = {
+  name: form.elements.name.value,
+  email: form.elements.email.value,
+  description: form.elements.description.value,
+};
+
+form.addEventListener("submit", (event) => {
+  if (validateEmail(email.value)) {
+    small.textContent = "";
   } else {
-    small.textContent = 'Please enter email address in small caps.';
+    small.textContent = "Please enter email address in small caps.";
     event.preventDefault();
   }
+  localStorage.setItem("formData", JSON.stringify(formData));
 });
+
+const displayData = () => {
+  const { name, email, message } = JSON.parse(localStorage.getItem("formData"));
+};
+
+displayData();
+
+// ---------LOCAL STORAGE-----------// END
