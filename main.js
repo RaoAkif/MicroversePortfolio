691cc33620d8671d4ea3e15dbcd18f66ec75128c
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-04 02:24:27 +0500

    Linter Errors

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const menuIcon = document.querySelector(".hamburger");
-const closeIcon = document.querySelector("#close-icon");
-const navBarList = document.querySelectorAll(".navbar-list-mobile li");
-const mobilePopupNav = document.querySelector("#mobile-popup");
+const menuIcon = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('#close-icon');
+const navBarList = document.querySelectorAll('.navbar-list-mobile li');
+const mobilePopupNav = document.querySelector('#mobile-popup');
 
-menuIcon.addEventListener("click", () => {
-  mobilePopupNav.classList.toggle("active");
+menuIcon.addEventListener('click', () => {
+  mobilePopupNav.classList.toggle('active');
 });
 
 navBarList.forEach((i) => {
-  i.addEventListener("click", () => {
-    mobilePopupNav.classList.toggle("active");
+  i.addEventListener('click', () => {
+    mobilePopupNav.classList.toggle('active');
   });
 });
 
-closeIcon.addEventListener("click", () => {
-  mobilePopupNav.classList.toggle("active");
+closeIcon.addEventListener('click', () => {
+  mobilePopupNav.classList.toggle('active');
 });
 
 // --------FORM VALIDATION----------
 
-const form = document.getElementById("form");
-const small = document.getElementsByTagName("small")[0];
-const name = document.getElementById("name");
-const email = document.getElementById("email-address");
-const description = document.getElementById("description");
+const form = document.getElementById('form');
+const small = document.getElementsByTagName('small')[0];
+const name = document.getElementById('name');
+const email = document.getElementById('email-address');
+const description = document.getElementById('description');
 
 function validateEmail(input) {
   const address = input.toString();
@@ -44,18 +44,18 @@ const formData = {
   description: form.elements.description.value,
 };
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   if (validateEmail(email.value)) {
-    small.textContent = "";
+    small.textContent = '';
   } else {
-    small.textContent = "Please enter email address in small caps.";
+    small.textContent = 'Please enter email address in small caps.';
     event.preventDefault();
   }
-  localStorage.setItem("formData", JSON.stringify(formData));
+  localStorage.setItem('formData', JSON.stringify(formData));
 });
 
 const displayData = () => {
-  const { name, email, message } = JSON.parse(localStorage.getItem("formData"));
+  const { name, email, message } = JSON.parse(localStorage.getItem('formData'));
 };
 
 displayData();
