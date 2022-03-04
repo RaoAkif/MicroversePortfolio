e25910e0ebbad92fefeb4cb0089a8693e25324a8
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-03-04 06:12:10 +0500

    Linter Error

--- main.js
+++ main.js
@@ -21,162 +21,163 @@ closeIcon.addEventListener('click', () => {
 
 const projects = [
   {
-    title: "Project 1: Keeping track of hundreds of components",
+    title: 'Project 1: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_1.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
   {
-    title: "Project 2: Keeping track of hundreds of components",
+    title: 'Project 2: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_2.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_2.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
   {
-    title: "Project 3: Keeping track of hundreds of components",
+    title: 'Project 3: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_3.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_3.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
   {
-    title: "Project 4: Keeping track of hundreds of components",
+    title: 'Project 4: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_4.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_4.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
   {
-    title: "Project 5: Keeping track of hundreds of components",
+    title: 'Project 5: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_5.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_5.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
   {
-    title: "Project 6: Keeping track of hundreds of components",
+    title: 'Project 6: Keeping track of hundreds of components',
     image:
-      "https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_6.png",
+      'https://raw.githubusercontent.com/RaoAkif/Portfolio/Project_Modal/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_6.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
     tags: {
-      tag1: "HTML",
-      tag2: "CSS",
-      tag3: "JS",
-      tag4: "React",
-      tag5: "Ruby",
+      tag1: 'HTML',
+      tag2: 'CSS',
+      tag3: 'JS',
+      tag4: 'React',
+      tag5: 'Ruby',
     },
-    linkLive: "#",
-    linkSource: "#",
+    linkLive: '#',
+    linkSource: '#',
   },
 ];
 
-const Portfolios = document.querySelector("#portfolios");
+const Portfolios = document.querySelector('#portfolios');
 
 projects.forEach((project, index) => {
-  console.log(project.image);
   Portfolios.innerHTML += `
-      <div class="portfolio">
-          <img class="portfolio-image" src=${
+      <div class='portfolio'>
+          <img class='portfolio-image' src=${
             project.image
-          } alt="portfolio_image_${index}" />
-          <div class="portfolio-desc">
-          <h2 class="project-title">${project.title}</h2>
-          <ul class="tags">
-          <li class="tag">${project.tags.tag1}</li>
-          <li class="tag">${project.tags.tag2}</li>
-          <li class="tag">${project.tags.tag3}</li>
-          <li class="tag">${project.tags.tag4}</li>
-          <li class="tag">${project.tags.tag5}</li>
+          } alt='portfolio_image_${index}' />
+          <div class='portfolio-desc'>
+          <h2 class='project-title'>${project.title}</h2>
+          <ul class='tags'>
+          <li class='tag'>${project.tags.tag1}</li>
+          <li class='tag'>${project.tags.tag2}</li>
+          <li class='tag'>${project.tags.tag3}</li>
+          <li class='tag'>${project.tags.tag4}</li>
+          <li class='tag'>${project.tags.tag5}</li>
           </ul>
-          <button type="button" id="see-project-${index + 1}" class="primary-button button-states see-project">See Project</button>
+          <button type='button' id='see-project-${
+            index + 1
+          }' class='primary-button button-states see-project'>See Project</button>
           </div>
       </div>
       `;
 });
 
-const seeProject = document.querySelectorAll(".see-project");
-const modalWindow = document.querySelector("#modal-window");
+const seeProject = document.querySelectorAll('.see-project');
+const modalWindow = document.querySelector('#modal-window');
 
 for (let i = 0; i < projects.length; i += 1) {
-    seeProject[i].addEventListener("click", () => {
+  seeProject[i].addEventListener('click', () => {
     const MobileModal = `
-      <div id="modal">
-        <img id="mobile-modal-image" src=${projects[i].image} alt="">
-        <img id="mobile-modal-close-icon" src="./images/icons/mobile-modal-close.svg" alt="close icon" onclick="closeModal()" >
-        <h2 id="mobile-modal-title">${projects[i].title}</h2>
-        <ul id="mobile-modal-tags">
-        <li class="tag">${projects[i].tags.tag1}</li>
-        <li class="tag">${projects[i].tags.tag2}</li>
-        <li class="tag">${projects[i].tags.tag3}</li>
-        <li class="tag">${projects[i].tags.tag4}</li>
-        <li class="tag">${projects[i].tags.tag5}</li>
+      <div id='modal'>
+        <img id='mobile-modal-image' src=${projects[i].image} alt=''>
+        <img id='mobile-modal-close-icon' src='./images/icons/mobile-modal-close.svg' alt='close icon' onclick='closeModal()' >
+        <h2 id='mobile-modal-title'>${projects[i].title}</h2>
+        <ul id='mobile-modal-tags'>
+        <li class='tag'>${projects[i].tags.tag1}</li>
+        <li class='tag'>${projects[i].tags.tag2}</li>
+        <li class='tag'>${projects[i].tags.tag3}</li>
+        <li class='tag'>${projects[i].tags.tag4}</li>
+        <li class='tag'>${projects[i].tags.tag5}</li>
         </ul>
-        <p id="mobile-modal-description">${projects[i].description}</p>
-        <ul id="links-project">
-        <li class="links" id="live-link"><a href=${projects[i].linkLive}>See Live<img class="link-icons" src="./images/icons/see-live.svg" alt=""></a></li>
-        <li class="links" id="source-link"><a href=${projects[i].linkSource}>See Source<img class="link-icons" src="./images/icons/see-source.svg" alt=""></a></li>
+        <p id='mobile-modal-description'>${projects[i].description}</p>
+        <ul id='links-project'>
+        <li class='links' id='live-link'><a href=${projects[i].linkLive}>See Live<img class='link-icons' src='./images/icons/see-live.svg' alt=''></a></li>
+        <li class='links' id='source-link'><a href=${projects[i].linkSource}>See Source<img class='link-icons' src='./images/icons/see-source.svg' alt=''></a></li>
         </ul>
         </div>`;
-        modalWindow.innerHTML = MobileModal;
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        modalWindow.classList.toggle('active');
+    modalWindow.innerHTML = MobileModal;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    modalWindow.classList.toggle('active');
   });
-}
+};
 
-const mobileModalCloseIcon = document.querySelector("#mobile-modal-close-icon");
+const mobileModalCloseIcon = document.querySelector('#mobile-modal-close-icon');
 
 function closeModal() {
-    modalWindow.removeChild(document.getElementById('modal'))
-    modalWindow.classList.toggle('active');
-  }
+  modalWindow.removeChild(document.getElementById('modal'));
+  modalWindow.classList.toggle('active');
+}
 
 // ----------------- Popup window functionality ----------------------- END//
 
@@ -203,5 +204,5 @@ form.addEventListener('submit', (event) => {
   } else {
     small.textContent = 'Please enter email address in small caps.';
     event.preventDefault();
-  }
+  };
 });
